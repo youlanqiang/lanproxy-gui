42fb866261f00f84fdec1eb40161db6e4402444c
youlanqiang <youlanqiang@hotmail.com>
2020-10-26 23:44:51 +0800

    添加样式

--- src/top/youlanqiang/lanproxy/Main.java
+++ src/top/youlanqiang/lanproxy/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("波塞冬内网穿透工具");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
