648834a3afb1bcdd5e3379723215e219a3cb7a61
youlanqiang <youlanqiang@hotmail.com>
2020-10-28 22:33:26 +0800

    使用maven构建javafx项目,以及使用jpackage打包为gui程序。

--- src/main/java/top/youlanqiang/lanproxy/Application.java
+++ src/main/java/top/youlanqiang/lanproxy/Application.java
@@ -1,12 +1,12 @@
 package top.youlanqiang.lanproxy;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Application extends javafx.application.Application {
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -17,8 +17,4 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
---
+++ src/main/java/top/youlanqiang/lanproxy/Main.java
@@ -0,0 +1,13 @@
+package top.youlanqiang.lanproxy;
+
+
+import javafx.application.Application;
+
+public class Main  {
+
+
+
+    public static void main(String[] args) {
+        Application.launch(top.youlanqiang.lanproxy.Application.class);
+    }
+}
